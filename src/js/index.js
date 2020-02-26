bdb48f3f2a69604a5168e96996ee42a0682051d5
lorelei47 <948866419@qq.com>
2020-02-26 23:50:36 +0800

    增加tabBar、增加图鉴

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,38 @@
 (function(win) {
     let cp = new CardPool(Cards);
 
+    hamburgerEvent("hamburger-container");
+
     document.querySelector('.pump:nth-of-type(1)').addEventListener('click', function() {
         tenPump(1);
     });
     document.querySelector('.pump:nth-of-type(2)').addEventListener('click', function() {
         tenPump(10);
     });
+
+    mapView(Cards);
+
+    function mapView(cardArray) {
+        if (!Array.isArray(cardArray)) {
+            return;
+        }
+        let mapBox = document.querySelector('.map-box');
+        mapBox = createMapItem(mapBox, cardArray.length);
+
+        let nodeList = mapBox.querySelectorAll('.map-card');
+        for (let i = 0, len = nodeList.length; i < len; i++) {
+            nodeList[i].style.backgroundImage = "url('src/card/" + cardArray[i].id + ".jpg')";
+            let index = Cards.findIndex(item => item.id == cardArray[i].id);
+            nodeList[i].setAttribute("level", Cards[index].level);
+        }
+        let item = document.getElementsByClassName('map-container');
+        for (let z = 0; z < item.length; z++) {
+            item[z].addEventListener("click", function() {
+                // enlargeCardEvent(this, '.map-card');
+                // setLevelShadow(this, '.map-card');
+            });
+        }
+    }
 
     function tenPump(num) {
         let box = document.querySelector('.box');
@@ -23,8 +49,8 @@
         let item = document.getElementsByClassName('card-container');
         for (let z = 0; z < item.length; z++) {
             item[z].addEventListener("click", function() {
-                enlargeCardEvent(this);
-                setLevelShadow(this);
+                enlargeCardEvent(this, '.back');
+                setLevelShadow(this, '.back');
                 this.querySelector('.cover').style.transform = 'rotatey(180deg)';
                 this.querySelector('.back').style.transform = 'rotatey(0deg)';
             });
@@ -51,25 +77,37 @@
         return div;
     }
 
-    function setLevelShadow(that) {
-        let level = that.querySelector('.back').getAttribute("level");
+    function createMapItem(node, num) {
+        if (node.tagName.toLocaleLowerCase() != 'div') {
+            return;
+        }
+        for (let i = 0; i < num; i++) {
+            let container = createDom(node, 'map-card-container');
+            createDom(container, 'map-card');
+            node.appendChild(container);
+        }
+        return node;
+    }
+
+    function setLevelShadow(that, className) {
+        let level = that.querySelector(className).getAttribute("level");
         switch (level) {
             case "1":
-                that.querySelector('.back').style.animation = "shadowWaveRed 2s ease-in-out infinite";
+                that.querySelector(className).style.animation = "shadowWaveRed 2s ease-in-out infinite";
                 break;
             case "2":
-                that.querySelector('.back').style.animation = "shadowWaveOrange 2s ease-in-out infinite";
+                that.querySelector(className).style.animation = "shadowWaveOrange 2s ease-in-out infinite";
                 break;
             case "3":
-                that.querySelector('.back').style.animation = "shadowWaveBlue 10s ease-in-out infinite";
+                that.querySelector(className).style.animation = "shadowWaveBlue 10s ease-in-out infinite";
                 break;
         }
     }
 
-    function enlargeCardEvent(that) {
+    function enlargeCardEvent(that, className) {
         let showView = document.querySelector('.show');
         let maskView = document.querySelector('.mask');
-        that.querySelector('.back').onclick = function(e) {
+        that.querySelector(className).onclick = function(e) {
             showView.style.opacity = "1";
             showView.style.zIndex = "3";
             showView.style.backgroundImage = this.style.backgroundImage;
@@ -98,6 +136,24 @@
             }
         }
 
+    }
+
+    function hamburgerEvent(className) {
+        let hbg = document.querySelector('.' + className);
+        let btn = hbg.querySelectorAll(".hamburger-item");
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].onclick = function() {
+                for (let j = 0; j < btn.length; j++) {
+                    if (i == j) {
+                        btn[j].style.backgroundColor = "rgb(140, 224, 123)";
+                        document.querySelector('.' + btn[j].getAttribute("name")).style.display = "flex";
+                    } else {
+                        btn[j].style.backgroundColor = "rgb(19, 113, 100)";
+                        document.querySelector('.' + btn[j].getAttribute("name")).style.display = "none";
+                    }
+                }
+            }
+        }
     }
 
     //添加css规则
